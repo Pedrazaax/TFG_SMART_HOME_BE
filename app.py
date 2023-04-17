058c4bc05b93f043448a916a7b7d9b762e3d5569
Pedrazaax <carlos.pedraza@alu.uclm.es>
2023-04-17 18:51:05 +0200

    CORS

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from controller import userController, auth_usersController, deviceController
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Iniciar servidor en local: python -m uvicorn app:app --reload
 # Documentación Swagger: /docs
 # Documentación Redocly: /redoc
+
+# Configuración CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Routers
 #app.mount("/static", StaticFiles(directory="static"), name="static")
