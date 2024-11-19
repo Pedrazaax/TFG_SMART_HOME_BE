0ef695d3143758bc47172e1b8dfe00cb916d6780
Pedrazaax <carlos.pedraza@alu.uclm.es>
2024-11-19 09:40:54 +0100

    - Controlador y servicio del historial

--- controller/historylogsController.py
+++ controller/historylogsController.py
@@ -11,8 +11,9 @@ app = APIRouter(prefix="/historylogs",
                         responses={404: {"detail":"Not found"}})
 
 # Endpoint para obtener los logs de HA (Home Assistant)
-@app.get("/getlogs")
-async def get_logs(user: User = Depends(current_user)):
+@app.get("/getLogbook/{startTime}&{endTime}")
+async def get_logs(startTime: str, endTime:str, user: User = Depends(current_user)):
+
     # Verificar si el usuario está autenticado a través del token JWT en la cabecera
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
@@ -30,16 +31,17 @@ async def get_logs(user: User = Depends(current_user)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Dominio no encontrado")
 
     try:
-        logs = await historylogsService.get_logs_from_ha(token, dominio)
-        print(logs)  # Imprime la respuesta de los logs
+        logs = await historylogsService.get_logs_from_ha(token, dominio, startTime, endTime)
+        print("Logs obtenidos: *********************************", logs)
         return logs
     except Exception as e:
         print("Error al obtener logs: ", e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 # Endpoint para obtener el historial de HA en un periodo específico
-@app.get("/gethistory/{timestamp}")
-async def get_history(timestamp: str, user: User = Depends(current_user)):
+@app.get("/getPeriod/{startTime}&{endTime}&{entity_id}")
+async def get_history(startTime: str, endTime:str, entity_id:str, user: User = Depends(current_user)):
+
     # Verificar si el usuario está autenticado a través del token JWT en la cabecera
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
@@ -57,7 +59,7 @@ async def get_history(timestamp: str, user: User = Depends(current_user)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Dominio no encontrado")
 
     try:
-        history = await historylogsService.get_history_from_ha(token, dominio, timestamp)
+        history = await historylogsService.get_history_from_ha(token, dominio, startTime, endTime, entity_id)
         return history
     except Exception as e:
         print("Error al obtener historial: ", e)
--- service/historylogsService.py
+++ service/historylogsService.py
@@ -3,24 +3,34 @@
 import httpx
 
 # Función para obtener logs de Home Assistant
-async def get_logs_from_ha(token: str, dominio: str):
+async def get_logs_from_ha(token: str, dominio: str, startTime: str, endTime: str):
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
-    url = f"{dominio}/api/logbook"
+
+    # Cadena time con startTime y endTime
+    time = f"{startTime}?end_time={endTime}"
+    print("Time: ", time)
+
+    url = f"{dominio}/api/logbook/{time}"
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
 
 # Función para obtener historial de Home Assistant
-async def get_history_from_ha(token: str, dominio: str, timestamp: str):
+async def get_history_from_ha(token: str, dominio: str, startTime: str, endTime: str):
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
-    url = f"{dominio}/api/history/period/{timestamp}"
+
+    # Cadena time con startTime y endTime
+    time = f"{startTime}?end_time={endTime}"
+    print("Time: ", time)
+
+    url = f"{dominio}/api/history/period/{time}"
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=headers)
         response.raise_for_status()
